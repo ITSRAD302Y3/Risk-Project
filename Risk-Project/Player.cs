9ae217ad7543d5acbaea5d737e470312c4818adc
S00168347 <fearonedm@outlook.com>
2018-03-11 20:27:47 +0000

    Orders Class

--- Risk-Project/Player.cs
+++ Risk-Project/Player.cs
@@ -5,11 +5,27 @@ using Microsoft.Xna.Framework;
 
 public class Player
 {
+    public enum State
+    {
+        Idle,
+        Playing,
+        Eliminated,
+        Winner
+    }
+
+    public State PlayerState { get; set; }
+
+    public Player()
+    {
+        this.PlayerState = State.Idle;
+    }
 
     public string Name { get; set; }
-    public List<TempUnit> Armies { get; set; }
+    public List<Unit> Armies { get; set; }
 
-    public List<TempTerritory> Territories { get; set; }
+    public List<Territory> Territories { get; set; }
+    
+    private List<Order> Orders { get; set; }
 
     public Color Color { get; set; }
 
@@ -17,30 +33,37 @@ public class Player
     {
         return "Player: " + Name + "Units:   " + Armies + "Territories Owned: " + Territories ;
     }
+    
+    public void Surrender()
+    {
+        PlayerState = State.Eliminated;
+    }
 
-    //public List<TempOrders> GetOrders()
-    //{
-        
-    //}
+    public List<Order> GetOrders()
+    {
+        return Orders;
+    }
 
     public void Reset()
     {
-        this.Reset();
+        Name = null;
+        Armies = null;
+        Territories = null;
     }
 
     public void ResetOrders()
     {
-        this.Reset();
+        Orders = null;
     }
     
 }
 
-public class TempUnit
+public class Unit
 {
     public int Amount { get; set; }
 }
 
-public class TempTerritory
+public class Territory
 {
     public int Amount { get; set; }
 }
@@ -50,7 +73,24 @@ public class PlayerAI: Player
 
 }
 
-public class TempOrders
+public class Order 
 {
+    public enum Action
+    {
+        Deploy,
+        Transfer
+    }
 
+    public int Amount { get; set; }
+
+    public string Name { get; set; }
+
+    public Action GetAction { get; set; }
+
+    public Territory Destination { get; set; }
+
+    public override string ToString()
+    {
+        return "Player: " + Name + "Units: " + Amount + "Destination: " + Destination;
+    }
 }
