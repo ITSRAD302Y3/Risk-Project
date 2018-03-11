0df17adf619c89bb82b5574176fab10d8a9deb8c
David Brennan <david.brennan@mail.itsligo.ie>
2018-03-11 22:02:50 +0000

    Added unit class.

---
+++ Risk-Project/World Objects/Unit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Risk_Project.World_Objects
+{
+    class Unit
+    {
+        public int Amount;
+
+        public Unit()
+        {
+
+        }
+    }
+}
