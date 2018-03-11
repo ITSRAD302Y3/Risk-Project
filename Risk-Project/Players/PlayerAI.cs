7ff6646ac5c39cabe593326e1f6f3f557048d5ec
David Brennan <david.brennan@mail.itsligo.ie>
2018-03-11 22:32:11 +0000

    Added PlayerAI class.

---
+++ Risk-Project/Players/PlayerAI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Risk_Project.Players
+{
+    class PlayerAI : Player
+    {
+        public PlayerAI()
+        {
+
+        }
+    }
+}
