21d39f977c6b1b201734d4ae9d5f73d5649b5e42
David Brennan <david.brennan@mail.itsligo.ie>
2018-03-11 21:59:36 +0000

    Added order class.

---
+++ Risk-Project/Players/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Risk_Project.Players
+{
+    class Order
+    {
+        #region Properties
+        public enum Action
+        {
+            Deploy,
+            Transfer
+        }
+
+        public string Name;
+        public int Amount;
+        public Action CurrentAction;
+        public Territory Destination;
+        #endregion
+
+        #region Constructor
+        public Order()
+        {
+
+        }
+        #endregion
+    }
+}
