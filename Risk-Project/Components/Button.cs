3fe038e67b58661f286683f40c97565ceff50cb6
David Brennan <david.brennan@mail.itsligo.ie>
2018-03-12 14:50:31 +0000

    Add button class.

---
+++ Risk-Project/Components/Button.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended;
+
+namespace Risk_Project.Components
+{
+    class Button : SimpleDrawableGameComponent
+    {
+        #region Properties
+        public string Name;
+        private Board.Action buttonAction;
+        #endregion
+
+        #region Constructor
+        public Button()
+        {
+
+        }
+        #endregion
+
+        #region Methods
+        public override void Draw(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Board.Action CheckAction()
+        {
+            // If I'm clicked.
+            return buttonAction;
+        }
+
+        public void Zoom(bool zoomIn)
+        {
+            if (zoomIn)
+            {
+                // Zoom camera in.
+            }
+            else
+            {
+                // Zoom camera out.
+            }
+        }
+        #endregion
+    }
+}
