4a4ecf50d2b34d42e05c95917434adb6a20c1179
David Brennan <david.brennan@mail.itsligo.ie>
2018-05-03 22:42:50 +0100

    Update GameRoot class. Cleanup code.

--- Risk-Project/Components/Board.cs
+++ Risk-Project/Components/Board.cs
@@ -178,7 +178,7 @@ namespace Risk_Project.Components
             }
         }
 
-        public void SetMoveOrder()
+        private void SetMoveOrder()
         {
 
         }
--- Risk-Project/GameRoot.cs
+++ Risk-Project/GameRoot.cs
@@ -16,6 +16,9 @@ namespace Risk_Project
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        private int _width = 1280;
+        private int _height = 720;
+
         #region Properties
         public static Dictionary<string, Texture2D> 
             TextureResource = new Dictionary<string, Texture2D>();
@@ -38,6 +41,9 @@ namespace Risk_Project
 
             IsMouseVisible = true;
             IsFixedTimeStep = false;
+
+            Window.Title = "Systems Design - CA Assignment";
+            Window.AllowAltF4 = false;
         }
 
         protected override void Initialize()
@@ -63,7 +69,6 @@ namespace Risk_Project
 
             CreateBoard();
         }
-
 
         protected override void UnloadContent()
         {
@@ -93,8 +98,26 @@ namespace Risk_Project
         #region Methods
         private void GameInit()
         {
+            // Set Window position to center screen
+            Window.Position = new Point(
+            (GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width / 2) -
+            (graphics.PreferredBackBufferWidth / 2),
+            (GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height / 2) -
+            (graphics.PreferredBackBufferHeight / 2));
+
             currentCamera = new Camera2D(GraphicsDevice);
 
+            InitPlayers();
+            CreatePlayers();
+        }
+
+        public static void GameRestart()
+        {
+
+        }
+
+        private void InitPlayers()
+        {
             // Initialize players
             Players = new List<Player>();
 
@@ -102,13 +125,6 @@ namespace Risk_Project
             {
                 Players.Add(new Player());
             }
-
-            CreatePlayers();
-        }
-
-        public static void GameRestart()
-        {
-
         }
 
         private void CreatePlayers()
